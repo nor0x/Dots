66cd15a9d3ef00b1b06bbbb743a5796573755b06
Johnny <nor0x@users.noreply.github.com>
2023-10-18 20:30:14 +0200

    action scripts updated with runner paths

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -1,10 +1,8 @@
 cd ..
 version=$(cat version.txt)
 
-cd src/
+cd /Users/runner/work/Dots/Dots/src/
 dotnet restore
-echo "Building Dots for macOS x64"
-dotnet msbuild -t:BundleApp -property:Configuration=Release -p:UseAppHost=true -p:RuntimeIdentifier=osx-x64
 echo "Building Dots for macOS arm64"
 dotnet msbuild -t:BundleApp -property:Configuration=Release -p:UseAppHost=true -p:RuntimeIdentifier=osx-arm64
 
@@ -15,6 +13,9 @@ cp -Rf bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/MacOS bin/Release/ne
 cp -Rf bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/MonoBundle bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents
 cp bin/Release/net8.0-macos/osx-arm64/Dots.app/Contents/PkgInfo bin/Release/net8.0-macos/osx-arm64/publish/Dots.app/Contents/
 
+echo "Building Dots for macOS x64"
+dotnet msbuild -t:BundleApp -property:Configuration=Release -p:UseAppHost=true -p:RuntimeIdentifier=osx-x64
+
 echo "Prepare App Bundle for x64"
 rm bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/MacOS/*.pkg
 cp Assets/AppIcon.icns bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/Resources/
@@ -23,13 +24,13 @@ cp -Rf bin/Release/net8.0-macos/osx-x64/Dots.app/Contents/MonoBundle bin/Release
 cp bin/Release/net8.0-macos/osx-x64/Dots.app/Contents/PkgInfo bin/Release/net8.0-macos/osx-x64/publish/Dots.app/Contents/
 
 echo "dittoing Dots for macOS x64"
-cd bin/Release/net8.0-macos/osx-x64/publish
+cd /Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-x64/publish
 ditto -c -k --sequesterRsrc --keepParent Dots.app Dots-$version-macos-x64.zip
 macosx64file=$(echo Dots-$version-macos-x64.zip)
 export macosx64file
 
 echo "dittoing Dots for macOS arm64"
-cd ../osx-arm64/publish
+cd /Users/runner/work/Dots/Dots/src/bin/Release/net8.0-macos/osx-arm64/publish
 ditto -c -k --sequesterRsrc --keepParent Dots.app Dots-$version-macos-arm64.zip
 macosarm64file=$(echo Dots-$version-macos-arm64.zip)
 export macosarm64file
--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -3,27 +3,29 @@ version=$(cat version.txt)
 
 cd src
 dotnet restore
+echo "Building Dots for Windows arm64"
+
 echo "Building Dots for Windows x64"
 dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-x64
-echo "Building Dots for Windows x86"
-dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-x86
-echo "Building Dots for Windows arm64"
-dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-arm64
 
 echo "zip Dots for Windows x64"
 cd bin/Release/net8.0/win-x64/publish
-zip -r Dots-$version-win-x64.zip Dots.exe
+7z a -tzip Dots-$version-win-x64.zip Dots.exe
 windowsx64file=$(echo Dots-$version-win-x64.zip)
 export windowsx64file
 
+echo "Building Dots for Windows x86"
+dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-x86
+
 echo "zip Dots for Windows x86"
 cd ../win-x86/publish
-zip -r Dots-$version-win-x86.zip Dots.exe
+7z a -tzip Dots-$version-win-x86.zip Dots.exe
 windowsx86file=$(echo Dots-$version-win-x86.zip)
 export windowsx86file
 
+dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-arm64
+
 echo "zip Dots for Windows arm64"
 cd ../win-arm64/publish
-zip -r Dots-w-$versionin-arm64.zip Dots.exe
-windowsarm64file=$(echo Dots-$version-win-arm64.zip)
+7z a -tzip Dots-$version-win-arm64.zip Dots.exe
 export windowsarm64file
