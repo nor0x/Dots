02e180d4c00fc353ae10643e03091724a125fae9
nor0x <nor0x@users.noreply.github.com>
2023-10-19 17:53:57 +0200

    version display on macOS

--- src/AboutWindow.axaml.cs
+++ src/AboutWindow.axaml.cs
@@ -9,6 +9,10 @@ using Avalonia.Styling;
 using Dots.Helpers;
 using Newtonsoft.Json.Linq;
 
+#if MACOS
+using Foundation;
+#endif
+
 namespace Dots
 {
     public partial class AboutWindow : Window
@@ -20,8 +24,15 @@ namespace Dots
         public AboutWindow()
         {
             InitializeComponent();
+#if WINDOWS
             var version = Assembly.GetEntryAssembly().GetName().Version;
             VersionRun.Text = version.ToString(3);
+#endif
+
+#if MACOS
+            var v = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleVersion")?.ToString();
+            VersionRun.Text = v;
+#endif
         }
 
         private void OpenSourceButton_Clicked(object? sender, Avalonia.Input.TappedEventArgs e)
