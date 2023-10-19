177f0bf7b9218bbc979bd57b95e4b0327b6bc9ac
Johnny <nor0x@users.noreply.github.com>
2023-10-19 16:24:35 +0200

    zip without subfolders

--- scripts/build-windows.sh
+++ scripts/build-windows.sh
@@ -8,7 +8,9 @@ echo "Building Dots for Windows x64"
 dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-x64
 
 echo "zip Dots for Windows x64"
-7z a -tzip Dots-$version-win-x64.zip  bin/Release/net8.0/win-x64/publish/Dots.exe
+mv bin/Release/net8.0/win-x64/publish/Dots.exe .
+7z a -tzip Dots-$version-win-x64.zip Dots.exe
+rm Dots.exe
 windowsx64file=$(echo Dots-$version-win-x64.zip)
 export windowsx64file
 
@@ -16,7 +18,9 @@ echo "Building Dots for Windows x86"
 dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-x86
 
 echo "zip Dots for Windows x86"
-7z a -tzip Dots-$version-win-x86.zip bin/Release/net8.0/win-x86/publish/Dots.exe
+mv bin/Release/net8.0/win-x86/publish/Dots.exe .
+7z a -tzip Dots-$version-win-x86.zip Dots.exe
+rm Dots.exe
 windowsx86file=$(echo Dots-$version-win-x86.zip)
 export windowsx86file
 
@@ -24,6 +28,8 @@ echo "Building Dots for Windows arm64"
 dotnet publish "Dots.csproj" -c Release -f net8.0 -r win-arm64
 
 echo "zip Dots for Windows arm64"
+mv bin/Release/net8.0/win-arm64/publish/Dots.exe .
 7z a -tzip Dots-$version-win-arm64.zip bin/Release/net8.0/win-arm64/publish/Dots.exe
+rm Dots.exe
 windowsarm64file=$(echo Dots-$version-win-arm64.zip)
 export windowsarm64file
