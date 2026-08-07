964904f0295f1e11237fd6fba374386487edacae
nor0x <nor0x@users.noreply.github.com>
2026-08-07 15:29:30 +0200

    housekeeping version string fix

--- scripts/housekeeping.sh
+++ scripts/housekeeping.sh
@@ -1,14 +1,20 @@
+set -e
+
 cd ..
 version="${RELEASE_VERSION:?RELEASE_VERSION must be set to a SemVer version}"
 cd src
 
-sed -i '' "s/CFBundleVersion>.*</CFBundleVersion>$version</" Dots.csproj
-sed -i '' "s/CFBundleShortVersionString>.*</CFBundleShortVersionString>$version</" Dots.csproj
-sed -i '' "s/Version>.*</Version>$version</" Dots.csproj
+if [[ "$OSTYPE" == darwin* ]]; then
+	sed_in_place=(-i '')
+else
+	sed_in_place=(-i)
+fi
 
-sed -i Dots.csproj -e "s/CFBundleVersion>.*</CFBundleVersion>$version</" 
-sed -i Dots.csproj -e "s/CFBundleShortVersionString>.*</CFBundleShortVersionString>$version</"
-sed -i Dots.csproj -e "s/Version>.*</Version>$version</"
+sed "${sed_in_place[@]}" \
+	-e "s|<CFBundleVersion>[^<]*</CFBundleVersion>|<CFBundleVersion>$version</CFBundleVersion>|" \
+	-e "s|<CFBundleShortVersionString>[^<]*</CFBundleShortVersionString>|<CFBundleShortVersionString>$version</CFBundleShortVersionString>|" \
+	-e "s|<Version>[^<]*</Version>|<Version>$version</Version>|" \
+	Dots.csproj
 
 echo "Version is now $version"
 # artifact names are decided by vpk at pack time (see build-windows.sh / build-macos.sh)
